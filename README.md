21fb76ae8f79a6da8d2c7a2d8a2a220ca106966d
rulamon <ruben.lamon@gmail.com>
2018-11-14 21:06:29 +0100

    own solution

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# FCC-scripting-finders-keepers" 
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/finders-keepers
---
+++ first.js
@@ -0,0 +1,8 @@
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++){
+    if(func(arr[i])){return arr[i]}
+  }
+  return undefined;
+}
+
+console.log(findElement([1, 900, 3, 5, 9], num => num % 2 === 0));
